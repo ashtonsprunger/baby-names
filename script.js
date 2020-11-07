5f3337c5ea975d7a9b5a654fe451f45bc08d5d1f
ashtonsprunger <sprungerashton@gmail.com>
2020-11-06 23:26:01 -0500

    bug-fix

--- script.js
+++ script.js
@@ -1,35 +1,76 @@
-let namesList = document.getElementById("names");
-let search = document.getElementById("search");
-let button = document.getElementById("random-button");
-let h = document.querySelector("h2");
+let namesUl = document.getElementById("names");
+let searchInput = document.getElementById("search");
+let randomButton = document.getElementById("random-button");
+let h2 = document.querySelector("h2");
+let pageInfo = document.getElementById("page-info");
+let buttons = document.getElementsByClassName("buttons")[0];
 
 let toggleButton = document.getElementById("toggle-button");
-let globalButton = document.getElementById("toggle-global");
+let searchButton = document.getElementById("search-button");
 let form = document.getElementById("form");
 
-let namesFile;
-let start = true;
-let showing = [];
+let namesPerPage = 50;
+let page = 0;
+
+let searchByStart = true;
+
+let currentNames = [];
+let visibleNames = [];
+
+let ALL_NAMES = [];
+
+fetch("./names.json")
+  .then((res) => res.json())
+  .then((json) => {
+    ALL_NAMES = json;
+    drawNames(ALL_NAMES);
+  });
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 });
 
 const toggleStart = () => {
-  start = !start;
-  toggleButton.innerHTML = start ? "START" : "ALL";
+  searchByStart = !searchByStart;
+  toggleButton.innerHTML = searchByStart ? "START" : "ALL";
+};
+
+const nextPage = () => {
+  if (namesPerPage * page + namesPerPage >= currentNames.length) {
+    console.log("NO");
+  } else {
+    page++;
+    window.scrollTo(0, 0);
+    drawNames(currentNames);
+  }
+};
+
+const previousPage = () => {
+  if (page > 0) {
+    page--;
+    window.scrollTo(0, 0);
+    drawNames(currentNames);
+  }
+};
+
+const firstPage = () => {
+  if (page > 0) {
+    page = 0;
+    window.scrollTo(0, 0);
+    drawNames(currentNames);
+  }
 };
 
 const filterNames = (query) => {
   let namesToShow = [];
-  if (start) {
-    namesFile.map((name) => {
+  if (searchByStart) {
+    ALL_NAMES.map((name) => {
       if (name.toLowerCase().startsWith(query.toLowerCase())) {
         namesToShow.push(name);
       }
     });
   } else {
-    namesFile.map((name) => {
+    ALL_NAMES.map((name) => {
       if (name.toLowerCase().includes(query.toLowerCase())) {
         namesToShow.push(name);
       }
@@ -38,47 +79,44 @@ const filterNames = (query) => {
   return namesToShow;
 };
 
-search.addEventListener("submit", () => {
-  console.log("enter");
-});
+// search.addEventListener("submit", () => {
+//   console.log("enter");
+// });
 
 const searchNames = () => {
+  page = 0;
   drawNames(filterNames(search.value));
 };
 
 const getRandomName = () => {
   let name;
-  if (showing.length > 0) {
-    name = showing[Math.floor(Math.random() * showing.length)];
+  if (currentNames.length > 0) {
+    name = currentNames[Math.floor(Math.random() * currentNames.length)];
   } else {
     name = "No names!";
   }
-  button.innerHTML = name;
+  randomButton.innerHTML = name;
 };
 
 const drawNames = (names) => {
   if (names.length == 0) {
-    h.innerHTML = `no results for '${search.value}'`;
+    h2.innerHTML = `no results for '${search.value}'`;
   } else {
-    h.innerHTML = `${names.length} ${names.length == 1 ? "result" : "results"}`;
+    h2.innerHTML = `${names.length} ${
+      names.length == 1 ? "result" : "results"
+    }, page ${page + 1}`;
   }
-  namesList.innerHTML = "";
-  names.map((name) => {
-    let li = document.createElement("li");
-    li.innerHTML = name;
-    namesList.appendChild(li);
-  });
-  showing = names;
+  namesUl.innerHTML = "";
+  names
+    .slice(page * namesPerPage, page * namesPerPage + namesPerPage)
+    .map((name) => {
+      let li = document.createElement("li");
+      li.innerHTML = name;
+      namesUl.appendChild(li);
+    });
+  pageInfo.innerHTML = `page ${page + 1} of ${Math.ceil(
+    names.length / namesPerPage
+  )}`;
+  buttons.style.display = names.length != 0 ? "grid" : "none";
+  currentNames = names;
 };
-
-var rawFile = new XMLHttpRequest();
-rawFile.open("GET", "./all.txt", false);
-rawFile.onreadystatechange = function () {
-  if (rawFile.readyState === 4) {
-    if (rawFile.status === 200 || rawFile.status == 0) {
-      namesFile = rawFile.responseText.split("\n");
-      drawNames(namesFile);
-    }
-  }
-};
-rawFile.send(null);
